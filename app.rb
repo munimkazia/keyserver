99c8edea39212f44836a366e60d92ea46e3b2783
Abdul Munim Kazia <munim2020@gmail.com>
2014-12-31 18:28:06 +0530

    listing out the api routes

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+get '/' do
+	'ok'
+end
+
+post '/keys' do
+	#generate keys
+end
+
+get '/key' do 
+
+end
+
+post '/key/release/:id' do
+
+end
+
+post '/key/delete/:id' do
+
+end
+
+post '/key/refresh/:id' do
+
+end
