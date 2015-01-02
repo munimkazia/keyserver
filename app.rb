1055bd61edee7b43d4e0a813c77bf2954e3839a2
Abdul Munim Kazia <munim2020@gmail.com>
2015-01-02 15:10:43 +0530

    updated routes

--- app.rb
+++ app.rb
@@ -1,25 +1,41 @@
 require 'sinatra'
+require './key_server.rb'
+
+key_server = KeyServer.new
+
+Thread.new do 
+  while true do
+     sleep 1
+     key_server.cleanup
+  end
+end
 
 get '/' do
 	'ok'
 end
 
-post '/keys' do
-	#generate keys
+get '/keys' do
+	key_server.generate 10, 300
+	'ok'
 end
 
 get '/key' do 
-
+	res = key_server.get
+	if res == nil
+		404
+	else
+		res
+	end
 end
 
-post '/key/release/:id' do
-
+get '/key/release/:id' do |key|
+	key_server.release key
 end
 
-post '/key/delete/:id' do
-
+get '/key/delete/:id' do |key|
+	key_server.delete key
 end
 
-post '/key/refresh/:id' do
-
+get '/key/refresh/:id' do |key|
+	key_server.refresh key
 end
